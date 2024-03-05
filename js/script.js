cddaed316633a550eea17903de78542a3c36f908
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-05 13:53:01 +0100

    [add] Objects Array

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const ourTeam = [
+  {
+    firstName: 'Wayne',
+    lastName: 'Barnett',
+    job: 'Founder & CEO',
+    img: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Caroll',
+    job: 'Chief Editor',
+    img: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    firstName: 'Walter',
+    lastName: 'Gordon',
+    job: 'Office Manager',
+    img: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    firstName: 'Angela',
+    lastName: 'Lopez',
+    job: 'Social Media Manager',
+    img: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    firstName: 'Scott',
+    lastName: 'Estrada',
+    job: 'Developer',
+    img: 'scott-estrada-developer.jpg',
+  },
+  {
+    firstName: 'Barbara',
+    lastName: 'Ramos',
+    job: 'Graphic Designer',
+    img: 'barbara-ramos-graphic-designer.jpg',
+  }
+]
